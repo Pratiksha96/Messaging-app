ae09ff3b9cc552d40d3574cf711ce154a2d4b27d
Pratiksha96 <jainpratiksha2110@gmail.com>
2019-09-04 14:47:27 +0530

    Added database to save messages.

--- server.js
+++ server.js
@@ -3,24 +3,37 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-var messages = [
-    { name: 'Tim', message: 'Hi' },
-    { name: 'Jane', message: 'Hello' }
-]
+var dbUrl = 'mongodb+srv://user:user@messaging-app-fbzmm.mongodb.net/test?retryWrites=true&w=majority'
+
+var Message =   mongoose.model('Message',{
+    name: String,
+    message: String
+})
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Message.find({},(err,messages)=>{
+        res.send(messages)
+    })
 })
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('message',req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+    message.save((err)=>{
+        if(err)
+            sendStatus(500)
+        io.emit('message', req.body)
+        res.sendStatus(200)
+    })  
+})
+
+mongoose.connect(dbUrl,{useNewUrlParser: true}, (err)=>{
+    console.log("mongo db connection ",err)
 })
 
 io.on('connection', (socket) => {
