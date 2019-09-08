7216affa0b102c9f55359f43e75e81f7ee3c91b8
Pratiksha96 <jainpratiksha2110@gmail.com>
2019-09-09 01:36:50 +0530

    Used promise to handle censored word.

--- server.js
+++ server.js
@@ -24,19 +24,22 @@ app.get('/messages', (req, res) => {
 
 app.post('/messages', (req, res) => {
     var message = new Message(req.body)
-    message.save((err)=>{
-        if(err)
-            sendStatus(500)
-        Message.findOne({message:'badword'},(err,censored)=>{
-            if(censored){
-                console.log("Censored word found!")
-                Message.deleteOne({ _id: censored.id }, (err) => {
-                    console.log("Removed censored word.")
-                    })
-                }
-            })
+    message.save()
+    .then(()=> {
+        console.log("saved")
+        return Message.findOne({message:'badword'})
+   })
+    .then(censored=>{
+        if (censored) {
+        console.log("Censored word found!")
+        return Message.remove({ _id: censored.id })
+        }
         io.emit('message', req.body)
         res.sendStatus(200)
+    })    
+    .catch((err) => {
+        res.sendStatus(500)
+        return console.error(err) 
     })  
 })
 
